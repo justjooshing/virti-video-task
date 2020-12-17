247bf5ace97057625a502a47624a2e3685c8583d
Joshua Mertens <joshua.l.mertens@gmail.com>
2020-12-17 18:41:40 +0000

    make everything *prettier* and fix controls to stay inside video container

--- src/components/MediaWrapper/MediaWrapper.jsx
+++ src/components/MediaWrapper/MediaWrapper.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
+import React from "react";
 
 import Video from "../Video/Video";
-import Images from "../Images/Images"
+import Images from "../Images/Images";
 
 export default function MediaWrapper(props) {
   return (
     <div className="media-wrapper">
-      <Video 
-        videoRef={props.videoRef} 
-        checkCount={props.checkCount}
-      />
-      <Images 
+      <Video videoRef={props.videoRef} checkCount={props.checkCount} />
+      <Images
         showImage1={props.showImage1}
         showImage2={props.showImage2}
         showImage3={props.showImage3}
       />
     </div>
-  )
-} 
+  );
+}
